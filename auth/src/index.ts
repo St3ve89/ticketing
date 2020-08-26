9a27b5258ed4e4d1d889455eb38fe5df2590366a
Istvan Acs <istvan.acs89@gmail.com>
2020-08-26 14:14:28 +0100

    Creating Route Handlers

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+import { currentUserRouter } from './routes/current-user';
+
 const app = express();
 app.use(json());
+
+app.use(currentUserRouter);
 
 app.get('/api/users/currentuser', (req, res) => {
   res.send('Hi There!');
