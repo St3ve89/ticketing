83f0451639f5857d8eff5cd6a49f00562a5e5eff
Istvan Acs <istvan.acs89@gmail.com>
2020-08-26 14:44:54 +0100

    Scaffolding routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,10 +2,16 @@ import express from 'express';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 
 const app = express();
 app.use(json());
 
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 app.use(currentUserRouter);
 
 app.get('/api/users/currentuser', (req, res) => {
