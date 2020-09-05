5aa0d36789bf2a3978be87f1ed0cddf787a4fdcb
Istvan Acs <istvan.acs89@gmail.com>
2020-09-05 10:16:29 +0100

    Add async error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
@@ -16,7 +17,7 @@ app.use(signoutRouter);
 app.use(signupRouter);
 app.use(currentUserRouter);
 
-app.get('*', () => {
+app.get('*', async (req, res) => {
   throw new NotFoundError();
 });
 
