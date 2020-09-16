b95b4a1604c7938adbdf4892f0e85c97fee0fd9a
Istvan Acs <istvan.acs89@gmail.com>
2020-09-16 19:06:30 +0100

    Accessing Env Variables in a Pod

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -37,6 +37,10 @@ app.get('/api/users/currentuser', (req, res) => {
 });
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined!');
+  }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
       useNewUrlParser: true,
