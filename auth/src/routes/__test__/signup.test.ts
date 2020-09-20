c9f8b0493553ff099f6e95ba6ad6634a6299af63
Istvan Acs <istvan.acs89@gmail.com>
2020-09-20 13:51:48 +0100

    Add tests for invalid inputs

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -11,3 +11,35 @@ it('should return a 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+it('should return a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('should return a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'p',
+    })
+    .expect(400);
+});
+
+it('should return a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com' })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({ password: 'password' })
+    .expect(400);
+});
