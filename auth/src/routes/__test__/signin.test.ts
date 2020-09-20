68952a6b83efcf1bd25721ed0306652987d6c747
Istvan Acs <istvan.acs89@gmail.com>
2020-09-20 15:05:33 +0100

    Add Tests Around Sign In Functionality

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,36 @@
+import request from 'supertest';
+import { it, expect } from '@jest/globals';
+import { app } from '../../app';
+
+it('should fail when a email that does not exist is supplied', async () => {
+  await request(app)
+    .post('/api/users/signin')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(400);
+});
+
+it('should fail when an incorrect password is supplied', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(201);
+
+  await request(app)
+    .post('/api/users/signin')
+    .send({ email: 'test@test.com', password: 'asdasdasdasd' })
+    .expect(400);
+});
+
+it('should respond with a cookie when given valid credentials', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(201);
+
+  const response = await request(app)
+    .post('/api/users/signin')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(200);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
