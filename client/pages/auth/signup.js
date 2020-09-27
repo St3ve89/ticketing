03d7cb08580dad762d04ab1da5275a09ede412cf
Istvan Acs <istvan.acs89@gmail.com>
2020-09-27 13:59:03 +0100

    Fetching Data During SSR

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
-export default () => {
+const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { doRequest, errors } = useRequest({
@@ -44,3 +44,5 @@ export default () => {
     </form>
   );
 };
+
+export default SignUp;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,14 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log('currentUser', currentUser);
   return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get('/api/users/currentuser');
+
+  return response.data;
+};
+
+export default LandingPage;
