c4b80146ff64c669de255ad895010be763aa24fa
Istvan Acs <istvan.acs89@gmail.com>
2020-09-29 10:28:58 +0100

    Passing Through the Cookies

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,14 +5,12 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
   if (typeof window === 'undefined') {
     const { data } = await axios.get(
       'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
       {
-        headers: {
-          Host: 'ticketing.dev',
-        },
+        headers: req.headers,
       }
     );
     return data;
