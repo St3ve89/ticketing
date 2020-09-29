f07c8308af09ef6273076aae8978c83f6512b4af
Istvan Acs <istvan.acs89@gmail.com>
2020-09-29 10:41:44 +0100

    A Reusable API Client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,26 +1,14 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log('currentUser', currentUser);
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers,
-      }
-    );
-    return data;
-  } else {
-    const { data } = await axios.get('/api/users/currentuser');
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get('/api/users/currentuser');
 
-    return data;
-  }
-
-  return {};
+  return data;
 };
 
 export default LandingPage;
