1c395bfe0c855e0e458b3bee8463579d35e1dc1a
Istvan Acs <istvan.acs89@gmail.com>
2020-10-18 17:36:46 +0100

    Setup tickets services

---
+++ tickets/src/index.ts
@@ -0,0 +1,25 @@
+import { app } from './app';
+import mongoose from 'mongoose';
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined!');
+  }
+
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('Connected to mongoDB');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000');
+  });
+};
+
+start();
