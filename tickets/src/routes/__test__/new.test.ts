4729e904bf5eb9a65c3d2a3bf47103ca3cc03c5f
Istvan Acs <istvan.acs89@gmail.com>
2020-11-02 15:01:49 +0000

    Creating the router

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,7 +1,12 @@
 import request from 'supertest';
+import { it, expect } from '@jest/globals';
 import { app } from '../../app';
 
-it('has a route handler listening to /api/tickets for post request', async () => {});
+it('has a route handler listening to /api/tickets for post request', async () => {
+  const response = await request(app).post('/api/tickets').send({});
+
+  expect(response.status).not.toEqual(404);
+});
 
 it('can only be accessed if the user is signed in', async () => {});
 
