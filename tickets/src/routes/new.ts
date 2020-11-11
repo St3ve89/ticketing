5a660d32f430bb373626876ba7c143f91d0a3e0a
Istvan Acs <istvan.acs89@gmail.com>
2020-11-11 18:36:25 +0000

    Validating Title and Price

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,22 @@
-import { requireAuth } from '@iacstickets/common';
+import { requireAuth, validateRequest } from '@iacstickets/common';
+import { body } from 'express-validator';
 import express, { Request, Response } from 'express';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  '/api/tickets',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater then 0'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  }
+);
 
 export { router as createTicketRouter };
