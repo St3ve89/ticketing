9906da16b9b4ef72080b01c7075b716811936622
Istvan Acs <istvan.acs89@gmail.com>
2020-11-28 19:41:56 +0000

    Complete Index Route Implementation

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import cookieSession from 'cookie-session';
 import { currentUser, errorHandler, NotFoundError } from '@iacstickets/common';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
 
 const app = express();
 app.set('trust proxy', true);
@@ -19,6 +20,7 @@ app.use(
 app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
