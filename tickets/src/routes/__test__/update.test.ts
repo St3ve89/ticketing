c330550ba96c232c273597fc363d36ac375b8baa
Istvan Acs <istvan.acs89@gmail.com>
2020-11-29 16:55:57 +0000

    Finish update tests

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -11,6 +11,7 @@ it('returns a 404 if the provided id does not exist', async () => {
     .send({ title: 'asdasd', price: 20 })
     .expect(404);
 });
+
 it('returns a 401 if the user is not authenticated', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
   await request(app)
@@ -18,6 +19,7 @@ it('returns a 401 if the user is not authenticated', async () => {
     .send({ title: 'asdasd', price: 20 })
     .expect(401);
 });
+
 it('returns a 401 if the user does not own the ticket', async () => {
   const response = await request(app)
     .post('/api/tickets/')
@@ -30,5 +32,42 @@ it('returns a 401 if the user does not own the ticket', async () => {
     .send({ title: 'xcvxcvbvn', price: 100 })
     .expect(401);
 });
-it('returns a 400 if the user provides an invalid title or price', async () => {});
-it('updates the ticket provided valid inputs', async () => {});
+
+it('returns a 400 if the user provides an invalid title or price', async () => {
+  const cookie = global.signin();
+  const response = await request(app)
+    .post('/api/tickets/')
+    .set('Cookie', cookie)
+    .send({ title: 'asdasd', price: 20 });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({ title: '', price: 20 })
+    .expect(400);
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({ title: 'sdfsdfd', price: -20 })
+    .expect(400);
+});
+it('updates the ticket provided valid inputs', async () => {
+  const cookie = global.signin();
+  const response = await request(app)
+    .post('/api/tickets/')
+    .set('Cookie', cookie)
+    .send({ title: 'asdasd', price: 20 });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({ title: 'zxcvzxcv', price: 100 })
+    .expect(200);
+
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send();
+  expect(ticketResponse.body.title).toEqual('zxcvzxcv');
+  expect(ticketResponse.body.price).toEqual(100);
+});
