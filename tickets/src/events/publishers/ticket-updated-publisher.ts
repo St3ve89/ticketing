f8de41940fb9f2d2045ee2d83058ce903f8434fd
Istvan Acs <istvan.acs89@gmail.com>
2021-01-16 15:37:59 +0000

    Add Ticket Update Publishing

---
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketUpdatedEvent } from '@iacstickets/common';
+
+export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+}
