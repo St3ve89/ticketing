4d530f5661fab36f7eaf5e6ddd3ba92314609b50
St3ve89 <istvan.acs89@gmail.com>
2021-01-28 17:05:08 +0000

    Add Order Expiration Times

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -12,6 +12,7 @@ import { Order } from '../models/order';
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
+const EXPIRATION_WINDOW_SECONDS = 15 + 60;
 
 router.post(
   '/api/orders',
@@ -42,11 +43,20 @@ router.post(
     }
 
     // Calculate an exparation date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
     // Build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
 
+    await order.save();
     // Publish an event saying that an order was created
-    res.send({});
+    res.status(201).send(order);
   }
 );
 
