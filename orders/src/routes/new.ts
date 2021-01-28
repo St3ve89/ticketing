9582d7607415424a6611d3f8aa43923155813494
St3ve89 <istvan.acs89@gmail.com>
2021-01-28 15:36:42 +0000

    Finding Reserved Tickets

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,11 +1,14 @@
 import {
+  BadRequestError,
   NotFoundError,
+  OrderStatus,
   requireAuth,
   validateRequest,
 } from '@iacstickets/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import mongoose from 'mongoose';
+import { Order } from '../models/order';
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
@@ -29,6 +32,23 @@ router.post(
       throw new NotFoundError();
     }
     // Make sure that this ticket is not already reserved
+    // Run query to look at all orders. Find an order where to ticket
+    // is the ticket we just found *and* the orders status is *not* cancelled.
+    // If we find an order from that means the ticket *is* reserved
+    const existingOrder = await Order.findOne({
+      ticket: ticket,
+      status: {
+        $in: [
+          OrderStatus.Created,
+          OrderStatus.AwaitingPayment,
+          OrderStatus.Complete,
+        ],
+      },
+    });
+
+    if (existingOrder) {
+      throw new BadRequestError('Ticket is already reserved ');
+    }
 
     // Calculate an exparation date for this order
 
