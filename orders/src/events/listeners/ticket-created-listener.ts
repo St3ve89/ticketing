93d01ff3aff9a888ea0d8207dece5d85001e81a6
St3ve89 <istvan.acs89@gmail.com>
2021-09-15 12:15:26 +0100

    Create group name const for prevent typo

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -1,8 +1,9 @@
 import { Listener, Subjects, TicketCreatedEvent } from '@iacstickets/common';
 import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
-  queueGroupName = 'orders-service';
+  queueGroupName = queueGroupName;
   onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
 }
